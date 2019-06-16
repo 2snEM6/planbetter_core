c3597aa91ec02ebcbeb8d912b3db5e76675f8942
Daniel Limia <daniel.limia@sage.com>
2019-06-16 17:41:33 +0200

    Fix contract errors and update to 0.5.0

--- contracts/HeritableWallet.sol
+++ contracts/HeritableWallet.sol
@@ -1,20 +1,19 @@
-pragma solidity ^0.4.10;
+pragma solidity ^0.5.0;
 
 import "HeritableWalletFactory.sol";
 
 contract HeritableWallet {
-
     HeritableWalletFactory public factory;
-    address public beneficiary;
-    address public owner;
+    address payable public beneficiary;
+    address payable public owner;
     mapping(address => uint8) public points;
-    mapping(address => address) public wallets;
+    mapping(address => address payable) public wallets;
     uint public lastCheckInTime;
     uint public checkInPeriod;
     uint public totalPoints = 0;
 
     /* constructor */
-    function HeritableWallet(address walletBeneficiary, address walletOwner, uint periodInDays) {
+    constructor(address payable walletBeneficiary, address payable walletOwner, uint periodInDays) public {
         factory = HeritableWalletFactory(msg.sender); // asume the creator is always the factory
         beneficiary = walletBeneficiary; // intended beneficiary
         owner = walletOwner; // who currently controls the wallet
@@ -23,43 +22,43 @@ contract HeritableWallet {
     }
 
     /* anyone can deposit funds by sending funds to the contract address */
-    function() payable {}
+    function() external payable {}
 
     modifier onlyOwner() {
-        if (msg.sender != owner && msg.sender != HeritableWallet(owner).getOwner()) throw;
+        if (msg.sender != owner && msg.sender != HeritableWallet(owner).getOwner()) revert();
         lastCheckInTime = now;
         _; // function body
     }
 
     modifier onlyHeir() {
-        if (points[msg.sender] == 0) throw;
+        if (points[msg.sender] == 0) revert();
         _; // function body
     }
 
     /* called by owner periodically to prove he is alive */
-    function checkIn() onlyOwner {}
+    function checkIn() public onlyOwner {}
 
     /* called by owner to change check in period */
-    function setCheckInPeriod(uint periodInDays) onlyOwner {
+    function setCheckInPeriod(uint periodInDays) public onlyOwner {
         checkInPeriod = periodInDays * 1 days;
     }
 
     /* called by owner to send funds with data to chosen destination */
-    function sendFunds(address destination, uint amount, bytes data) onlyOwner {
-        if (!destination.call.value(amount)(data)) throw;
+    function sendFunds(address payable destination, uint amount) public onlyOwner {
+        destination.transfer(amount);
     }
 
     /* called by owner to change ownership */
-    function transferOwnership(address newOwner) onlyOwner {
+    function transferOwnership(address payable newOwner) public onlyOwner {
         owner = newOwner;
         beneficiary = owner;
     }
 
     /* called by owner to add/modify an heir; inheritance shares are directly proportional to the points assigned */
-    function setHeir(address heir, uint8 inheritancePoints, uint periodInDays) onlyOwner returns (address) {
-        if (wallets[heir] == 0 && inheritancePoints > 0) {
+    function setHeir(address payable heir, uint8 inheritancePoints, uint periodInDays) public onlyOwner returns (address payable) {
+        if (wallets[heir] == address(0) && inheritancePoints > 0) {
             wallets[heir] = factory.create(heir, periodInDays);
-        } else if (wallets[heir] > 0 && inheritancePoints == 0) {
+        } else if (wallets[heir] != address(0) && inheritancePoints == 0) {
             HeritableWallet(wallets[heir]).destroy();
             delete wallets[heir];
         }
@@ -70,25 +69,25 @@ contract HeritableWallet {
     }
 
     /* called by anyone to give the beneficiary full ownership of this account when his predecessor is inactive */
-    function unlock() {
-        if (beneficiary == owner) throw; // already unlocked
+    function unlock() public {
+        if (beneficiary == owner) revert(); // already unlocked
         HeritableWallet(owner).claimInheritance();
         owner = beneficiary;
         lastCheckInTime = now;
     }
 
     /* called by an heir to collect his share in the inheritance */
-    function claimInheritance() onlyHeir {
-        if (beneficiary != owner) throw; // account is locked
-        if (now <= lastCheckInTime + checkInPeriod) throw; // owner was active recently
+    function claimInheritance() public onlyHeir {
+        if (beneficiary != owner) revert(); // account is locked
+        if (now <= lastCheckInTime + checkInPeriod) revert(); // owner was active recently
         uint8 heirPoints = points[msg.sender];
-        uint amount = this.balance * heirPoints / totalPoints; // compute amount for current heir
+        uint amount = address(this).balance * heirPoints / totalPoints; // compute amount for current heir
         totalPoints -= heirPoints;
         delete points[msg.sender];
         if (!msg.sender.send(amount)) { // transfer proper amount to heir or revert state if it fails
             totalPoints += heirPoints;
             points[msg.sender] = heirPoints;
-            throw;
+            revert();
         }
         if (totalPoints == 0) { // last heir, destroy empty contract
             selfdestruct(owner);
@@ -96,11 +95,11 @@ contract HeritableWallet {
     }
 
     /* called by owner to terminate this contract */
-    function destroy() onlyOwner {
+    function destroy() public onlyOwner {
         selfdestruct(owner);
     }
 
-    function getOwner() view returns (address) { return owner; }
-    function getBeneficiary() view returns (address) { return beneficiary; }
+    function getOwner() public view returns (address) { return owner; }
+    function getBeneficiary() public view returns (address) { return beneficiary; }
 
 }
--- contracts/HeritableWalletFactory.sol
+++ contracts/HeritableWalletFactory.sol
@@ -1,22 +1,21 @@
-pragma solidity ^0.4.10;
+pragma solidity ^0.5.0;
 
 import "HeritableWallet.sol";
 
 contract HeritableWalletFactory {
-
     address public creator;
     mapping(address => address[]) public contracts;
 
     event WalletCreated(address walletAddress, address walletOwner, address walletBeneficiary);
 
-    function HeritableWalletFactory() {
+    constructor() public {
         creator = msg.sender;
     }
 
-    function create(address beneficiary, uint periodInDays) returns (address wallet) {
-        wallet = new HeritableWallet(beneficiary, msg.sender, periodInDays);
+    function create(address payable beneficiary, uint periodInDays) public returns (address payable wallet) {
+        wallet = address(new HeritableWallet(beneficiary, msg.sender, periodInDays));
         contracts[beneficiary].push(wallet);
-        WalletCreated(wallet, msg.sender, beneficiary);
+        emit WalletCreated(wallet, msg.sender, beneficiary);
     }
-
 }
+
